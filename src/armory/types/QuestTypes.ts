7e673b45d6446e443ee225779d376dfbeef864fa
smthbh <smthbh@gmail.com>
2025-04-29 09:03:28 -0400

    indent with tabs for linter

--- src/armory/types/QuestTypes.ts
+++ src/armory/types/QuestTypes.ts
@@ -1,17 +1,17 @@
 export interface IQuest {
-    id: number;
-    title: string;
-    status: 'Completed' | 'In Progress';
-    minLevel: number;
-    questLevel: number;
-    questSortID: number;
+	id: number;
+	title: string;
+	status: 'Completed' | 'In Progress';
+	minLevel: number;
+	questLevel: number;
+	questSortID: number;
 }
 
 export interface IQuestComparison {
-    id: number;
-    title: string;
-    questLevel: number;
-    char1Status?: 'Completed' | 'In Progress';
-    char2Status?: 'Completed' | 'In Progress';
-    category?: string;
+	id: number;
+	title: string;
+	questLevel: number;
+	char1Status?: 'Completed' | 'In Progress';
+	char2Status?: 'Completed' | 'In Progress';
+	category?: string;
 }
